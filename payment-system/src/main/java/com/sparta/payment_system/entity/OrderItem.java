65a1cd809f777782671d59277240e2478cc0a4f0
sjww0306 <sjww03@gmail.com>
2025-11-18 16:39:00 +0900

    reafactor : Order 클래스 내 타입 수정 및 리팩토링

--- payment-system/src/main/java/com/sparta/payment_system/entity/OrderItem.java
+++ payment-system/src/main/java/com/sparta/payment_system/entity/OrderItem.java
@@ -1,12 +1,19 @@
 package com.sparta.payment_system.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.math.BigDecimal;
 
 @Entity
 @Table(name = "order_items")
