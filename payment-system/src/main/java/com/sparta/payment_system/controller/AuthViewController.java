4bac471d2c07512df77608f57c2393487dc1068a
Juwoojae <juwqq1234@naver.com>
2025-11-22 15:45:47 +0900

    feat: registerForm 을 띄워줄 컨트롤러 구현 - AuthViewController 컨트롤러 에 registerForm 메서드 추가

--- payment-system/src/main/java/com/sparta/payment_system/controller/AuthViewController.java
+++ payment-system/src/main/java/com/sparta/payment_system/controller/AuthViewController.java
@@ -20,4 +20,9 @@ public class AuthViewController {
     public String loginForm() {
         return "loginForm";
     }
+
+	@GetMapping("/register")
+	public String registerForm() {
+		return "registerForm";
+	}
 }
